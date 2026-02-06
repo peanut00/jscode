79e434324fea9f49a66c848a4f0c6ec555a80978
peanut00 <abdelouahhab.jamai@gmail.com>
2026-02-06 19:17:01 +0100

    Syntax (fonction immédiatement invoquée IIFE)

--- nperf_dash.js
+++ nperf_dash.js
@@ -10,7 +10,7 @@
    
    ════════════════════════════════════════════════════════════════ */
 
-function(){
+(function(){
   /* Remove existing overlay if present */
   const existing = document.getElementById('nperf-custom-dashboard-overlay');
   if (existing) {
@@ -275,4 +275,4 @@ function(){
       }
     };
   }
-}();
+})();
